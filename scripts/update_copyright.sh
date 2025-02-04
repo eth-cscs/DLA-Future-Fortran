d23f9d20bb37264a6e4c3b228ca7204b740b6eac
Rocco Meli <rocco.meli@cscs.ch>
2025-02-04 14:09:29 +0000

    add copyright to update script

--- scripts/update_copyright.sh
+++ scripts/update_copyright.sh
@@ -1,4 +1,14 @@
 #!/bin/bash!
 
+#
+# Distributed Linear Algebra with Future (DLAF)
+#
+# Copyright (c) 2018-2025, ETH Zurich
+# All rights reserved.
+#
+# Please, refer to the LICENSE file in the root directory.
+# SPDX-License-Identifier: BSD-3-Clause
+#
+
 year=$(date +%Y)
 find ${PWD} -type f -exec sed -i "s/2018-2025/2018-${year}/g" {} +
