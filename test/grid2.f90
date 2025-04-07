ace201b5cfc09465c3c078985c09cb35c609f2c0
Rocco Meli <rocco.meli@cscs.ch>
2025-04-07 09:58:42 +0200

    Allow to free all DLA-Future grids (#34)
    Co-authored-by: Mikael Simberg <mikael.simberg@iki.fi>

---
+++ test/grid2.f90
@@ -0,0 +1,45 @@
+!
+! Distributed Linear Algebra with Future (DLAF)
+!
+! Copyright (c) 2018-2025, ETH Zurich
+! All rights reserved.
+!
+! Please, refer to the LICENSE file in the root directory.
+! SPDX-License-Identifier: BSD-3-Clause
+!
+
+program grid2
+   use dlaf_fortran, only: dlaf_initialize, dlaf_finalize
+   use dlaf_fortran, only: dlaf_create_grid_from_blacs, dlaf_free_all_grids
+   use testutils, only: setup_mpi, teardown_mpi
+
+   implicit none
+
+   external blacs_get
+   external blacs_gridinit
+
+   integer :: rank, numprocs
+   integer:: nprow, npcol
+   integer :: ictxt_0, ictxt_1
+
+   nprow = 2
+   npcol = 3
+
+   call setup_mpi(nprow, npcol, rank, numprocs)
+
+   ! Get default system context
+   call blacs_get(0, 0, ictxt_0)
+   call blacs_get(0, 0, ictxt_1)
+
+   call blacs_gridinit(ictxt_0, 'R', nprow, npcol)
+   call blacs_gridinit(ictxt_1, 'R', nprow, npcol)
+
+   call dlaf_initialize()
+   call dlaf_create_grid_from_blacs(ictxt_0)
+   call dlaf_create_grid_from_blacs(ictxt_1)
+   call dlaf_free_all_grids()
+   call dlaf_finalize()
+
+   call teardown_mpi()
+
+end program grid2
--- test/init.f90
+++ test/init.f90
@@ -8,7 +8,7 @@
 ! SPDX-License-Identifier: BSD-3-Clause
 !
 
-program hello
+program init
    use dlaf_fortran, only: dlaf_initialize, dlaf_finalize
    use testutils, only: setup_mpi, teardown_mpi
 
@@ -22,9 +22,16 @@ program hello
 
    call setup_mpi(nprow, npcol, rank, numprocs)
 
+   ! You can call dlaf_initialize() multiple times
+   ! If DLA-Future is already initialized, dlaf_initialize() does nothing
    call dlaf_initialize()
+   call dlaf_initialize()
+
+   ! You can call dlaf_finalize() multiple times
+   ! If DLA-Future is not initialized, dlaf_finalize() does nothing
+   call dlaf_finalize()
    call dlaf_finalize()
 
    call teardown_mpi()
 
-end program hello
+end program init
